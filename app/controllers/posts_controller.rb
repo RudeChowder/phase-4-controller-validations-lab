22bce13cd0f54511bd8a3d838e9bb6ac29c34a42
Stephen Crane <scrane88@gmail.com>
2021-09-10 13:16:12 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -19,5 +20,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  def render_unprocessable_entity_response(e)
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end 
 
 end
